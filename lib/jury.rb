1ab308c658dc2cc290b85ccfea90472bc69edf18
Chris Saden <chris.saden@udacity.com>
2016-05-03 01:44:14 -0700

    remove parens on method definitions

--- lib/jury.rb
+++ lib/jury.rb
@@ -9,7 +9,7 @@ class Jury
     @members << member
   end
 
-  def cast_votes(finalists)
+  def cast_votes finalists
     votes = members.inject({}) do |result, member|
       vote_for = finalists.sample
       result[vote_for] = 0 if result[vote_for].nil?
@@ -19,13 +19,13 @@ class Jury
     end
   end
 
-  def report_votes(final_votes)
+  def report_votes final_votes
     final_votes.each do |finalist, votes|
       puts "#{finalist.name} got #{votes} votes."
     end
   end
 
-  def announce_winner(final_votes)
+  def announce_winner final_votes
     p final_votes.key final_votes.values.max
   end
 end
